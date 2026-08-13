2fbdec0bc12eec00f01160c620198562ffbee020
MachadoEduardo <05eduardomachado@gmail.com>
2026-08-13 09:40:12 -0300

    refactore: desacoplamento de responsabilidades e organização de código

---
+++ src/features/groups/groups-page.tsx
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { GroupsList } from "@/features/groups/components/groups-list";
+import { GroupsPageHeader } from "@/features/groups/components/groups-page-header";
+import { groupsListQuery } from "@/features/groups/queries";
+
+export function GroupsPage() {
+  const groupsQuery = useQuery(groupsListQuery());
+
+  return (
+    <div className="space-y-8">
+      <GroupsPageHeader />
+      <GroupsList query={groupsQuery} />
+    </div>
+  );
+}
--- src/features/groups/queries.ts
+++ src/features/groups/queries.ts
@@ -3,6 +3,13 @@ import { api } from "@/lib/api";
 import { GROUP_LIVE_REFRESH_MS } from "@/lib/query-config";
 import { queryKeys } from "@/lib/query-keys";
 
+export function groupsListQuery() {
+  return queryOptions({
+    queryKey: queryKeys.groups.all(),
+    queryFn: () => api.groups.list({ limit: 50 }),
+  });
+}
+
 export function groupDetailQuery(groupId: string) {
   return queryOptions({
     queryKey: queryKeys.group.detail(groupId),
